81f40213898939d2b52bcb57faac2442142c681e
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-22 21:04:19 +0200

    inizio a lavorare su aggiunta carrello

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const inputSearch = document.querySelector('#inputSearch')
 const cardRow =document.querySelector('#cardRow')
+let cardCart = document.querySelector('#cardCartButton')
+console.log(cardCart)
 
-function getBooks(url) {
+let booksResult
+
+function getBooks() {
 
     fetch('https://striveschool-api.herokuapp.com/books/')
         .then(response => response.json())
@@ -16,13 +20,19 @@ function getBooks(url) {
             const renderedResult = booksResult.map(result => {
                 return /*html*/`
             <div class="col">
-                <div class="card h-100">
+                <div class="card h-100 border border-0 position-relative">
+                    
                     <img src="${result.img}" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title">${result.title}</h5>
-                        <p class="card-text">${result.price}€</p>
-                        <p class="card-text">Category: ${result.category}</p>
-                        <button type="button" class="btn btn-success"><i class="bi bi-cart2"></i>Aggiungi al carrrello</button>
+                        <h5 class="card-title text-center">${result.title}</h5>
+                        <p class="card-text text-center">${result.price}€</p>
+                        
+                    </div>
+                    <div class= "cart-overlay bg-light d-flex justify-content-evenly">
+                    <button type="button" class="btn btn-transparent"> <i class="bi bi-suit-heart fs-3"></i></button>
+
+                      <button type="button" id="cardCartButton" class="btn btn-transparent">   <i class="bi bi-cart2 fs-3"></i> </button>
+
                     </div>
                 </div>
             </div>
@@ -34,11 +44,66 @@ function getBooks(url) {
 
             })
             cardRow.innerHTML += renderedResult.join('')
-            
+            cardCart = document.querySelector('#cardCartButton')
         })
         .catch(error => console.log('error', error));
 }
 getBooks()
+
+
+
+
+// for (const button of cardCart) {
+
+//     button.addEventListener('click',(event)=>{
+
+//        console.log('cucuucuc')
+//     })
+    
+// }
+
+
+// function search(){
+    
+//     cardRow.innerHTML= ''
+//     booksResult.includes(()=>{
+    
+//     inputSearch.value === result.title
+// })
+
+
+    
+// const renderedResult = booksResult.map(result => {
+//     return /*html*/`
+// <div class="col">
+//     <div class="card h-100 border border-0 position-relative">
+        
+//         <img src="${result.img}" class="card-img-top" alt="...">
+//         <div class="card-body">
+//             <h5 class="card-title text-center">${result.title}</h5>
+//             <p class="card-text text-center">${result.price}€</p>
+            
+//         </div>
+//         <div class= "cart-overlay bg-light d-flex justify-content-evenly">
+//         <button type="button" class="btn btn-transparent"> <i class="bi bi-suit-heart fs-3"></i></button>
+
+//           <button type="button" class="btn btn-transparent">   <i class="bi bi-cart2 fs-3"></i> </button>
+
+//         </div>
+//     </div>
+// </div>
+
+
+      
+//       `
+
+
+// })
+// cardRow.innerHTML = renderedResult.join('')
+
+
+
+// }
 
         
             
