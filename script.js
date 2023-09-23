e23b4bd0bc10a494a80a6169e4822ac85a8dcb86
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-23 22:39:11 +0200

    problema funzione che lavora solo da console nel browser

--- script.js
+++ script.js
@@ -1,77 +1,107 @@
+
+
 const inputSearch = document.querySelector('#inputSearch')
 const cardRow =document.querySelector('#cardRow')
 let cardCart = document.querySelectorAll('.cartButton')
 let cartCount = document.querySelectorAll('.badge')
 let booksResult
 
-
 // funzione al click del carrello che aggiunge la quantita sul carrello principale
-
-    cardCart.forEach(button =>{
-        let count=0
-        
-        button.addEventListener('click', event =>{
-            count++
-            console.log(count)
-            cartCount =  document.querySelectorAll('.badge')
-            for (const cart of cartCount) {
-                cart.innerHTML = count
-                
-            }
-        })
-    })
     
+cardCart.forEach(button =>{
+    let count=0
+
+    button.addEventListener('click', event =>{
+
+        count++
+        console.log(count)
+        cartCount =  document.querySelectorAll('.badge')
+        for (const cart of cartCount) {
+            cart.innerHTML = count
+            
+            // const cartItems = booksResult.map( result =>{ return /*html */`
+            //                 <li class="list-group-item d-flex justify-content-between align-items-center border border-0 border-bottom">
+            //                     <div class="card mb-3" style="max-width: 540px;">
+            //                         <div class="row g-0">
+            //                             <div class="col-md-4">
+            //                                 <img src="${result.img}" class="img-fluid rounded-start" alt="...">
+            //                             </div>
+            //                             <div class="col-md-8">
+            //                                 <div class="card-body">
+            //                                     <h5 class="card-title">Card title</h5>
+            //                                     <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+            //                                 </div>
+            //                             </div>
+            //                         </div>
+            //                     </div>  
+            //                     <span class="badge bg-primary rounded-pill"></span>
+            //                 </li>
+            
+            
+            // `})
+            // const cartList = document.querySelector('#cartList')
+            // cartList.innerHTML += cartItems
+            
+        }
+    })
+})
+
 
 
 // funzione di chiamata di tutti i libri
 
 function getBooks() {
-
+    
     fetch('https://striveschool-api.herokuapp.com/books/')
-        .then(response => response.json())
-        .then((result) => {
-           
-            
-
-
-            booksResult = result
-
-
-            const renderedResult = booksResult.map(result => {
-                return /*html*/`
+    .then(response => response.json())
+    .then((result) => {
+        
+        
+        
+        
+        booksResult = result
+        
+        
+        const renderedResult = booksResult.map(result => {
+            return /*html*/`
             <div class="col">
-                <div class="card h-100 border border-0 position-relative">
-                    
-                    <img src="${result.img}" class="card-img-top" alt="...">
-                    <div class="card-body">
+            <div class="card h-100 border border-0 position-relative">
+            
+            <img src="${result.img}" class="card-img-top" alt="...">
+            <div class="card-body">
                         <h5 class="card-title text-center">${result.title}</h5>
                         <p class="card-text text-center">${result.price}€</p>
                         
-                    </div>
-                    <div class= "cart-overlay bg-light d-flex justify-content-evenly">
-                    <button type="button" class="btn btn-transparent"> <i class="bi bi-suit-heart fs-3"></i></button>
+                        </div>
+                        <div class= "cart-overlay bg-light d-flex justify-content-evenly">
+                        <button type="button" class="btn btn-transparent"> <i class="bi bi-suit-heart fs-3"></i></button>
+                        
+                        <button type="button" class="cartButton btn btn-transparent">   <i class="bi bi-cart2 fs-3"></i> </button>
+                        
+                        </div>
+                        </div>
+                        </div>
+                        
+                        
+                        
+                        `
+                        
+                        
+                    })
+                    cardRow.innerHTML += renderedResult.join('')
+                    cardCart = document.querySelectorAll('.cartButton')
+                    cartCount = document.querySelectorAll('.badge')
 
-                      <button type="button" class="cartButton btn btn-transparent">   <i class="bi bi-cart2 fs-3"></i> </button>
+                })
+        .catch(error => console.log('error', error));
+    }
+    getBooks()
+    
+    
 
-                    </div>
-                </div>
-            </div>
 
    
-                  
-                  `
-
-
-            })
-            cardRow.innerHTML += renderedResult.join('')
-            cardCart = document.querySelectorAll('.cartButton')
-        })
-        .catch(error => console.log('error', error));
-}
-getBooks()
-
-
-
+    
 
     
 
