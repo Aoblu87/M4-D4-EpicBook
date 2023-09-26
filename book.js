18ca53ead35aea38bb41078c312e26b9dd55b86a
Stefania <126720391+Aoblu87@users.noreply.github.com>
2023-09-26 20:48:08 +0200

    sistemato funzione addevent listners

--- book.js
+++ book.js
@@ -10,41 +10,48 @@ fetch(`https://striveschool-api.herokuapp.com/books/${id}`)
 const bookCard = document.querySelector("#cardBook")
 
 function displayBook(data) {
+    console.log(data.title)
+
+    const bookDetails = data.map(result => {
+        return /*html*/`
+        <div class="col d-flex">
+        <img src="${result.img}" class="img-fluid" alt="...">
+        </div>
+        <div class="col">
+        <div class="row">
+            <div class="col d-flex ">
+                <h5 class="">${result.title}</h5>
+                <h6 class="">${result.price}</h6>
+        
+            </div>
+            <div class="col">
+                <button class="btn btn-outline-secondary dropdown-toggle" type="button"
+                    data-bs-toggle="dropdown" aria-expanded="false">1</button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <select class="form-select" size="3" aria-label="Size 3 select example">
+                        <option selected>1</option>
+                        <option value="1">2</option>
+                        <option value="2">3</option>
+                        <option value="3">4</option>
+                    </select>
+                </ul>
+                <button type="button" class="btn btn-success">Aggiungi al carrello</button>
+        
+        
+            </div>
+        </div>
+        </div>
+                        
+                        
+                        
+        `
 
 
-    const bookDetails = data.map(result =>  /*html */
+        console.log(bookDetails)
 
-        `<div class="col d-flex">
-                        <img src="${result.img}" class="img-fluid" alt="...">
-                    </div>
-                    <div class="col">
-                        <div class="row">
-                            <div class="col d-flex ">
-                                <h5 class="">${result.title}</h5>
-                                <h6 class="">${result.price}</h6>
-    
-                            </div>
-                            <div class="col">
-                                <button class="btn btn-outline-secondary dropdown-toggle" type="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">1</button>
-                                <ul class="dropdown-menu dropdown-menu-end">
-                                    <select class="form-select" size="3" aria-label="Size 3 select example">
-                                        <option selected>1</option>
-                                        <option value="1">2</option>
-                                        <option value="2">3</option>
-                                        <option value="3">4</option>
-                                    </select>
-                                </ul>
-                                <button type="button" class="btn btn-success">Aggiungi al carrello</button>
-    
-    
-                            </div>
-                        </div>
-                    </div>`
-
-
-    )
+    })
 
     bookCard.innerHTML += bookDetails.join('')
 
 }
+
--- script.js
+++ script.js
@@ -4,30 +4,79 @@ let inputSearch = document.querySelector('#inputSearch')
 let cardRow = document.querySelector('#cardRow')
 let addItems = document.querySelectorAll('.cartButton')
 let cartCount = document.querySelectorAll('.badge')
-let booksResult
+let booksResult =[]
+let renderedResult
+let books
 
 // funzione al click del carrello che aggiunge la quantita sul carrello principale
+console.log(addItems)
+function addToCart (button){
+    
+    let count=0
 
-// cardCart.forEach(button =>{
-//     let count=0
+    button.addEventListener('click', event =>{
+        let cartList = document.querySelector('#cartList')
 
-//     button.addEventListener('click', event =>{
+        const idBook = button.getAttribute('data-asin')
+        
+        const bookSelected = booksResult.find( book => book.asin === idBook)
 
-//         count++
-//         console.log(count)
-//         cartCount =  document.querySelectorAll('.badge')
-//         for (const cart of cartCount) {
-//             cart.innerHTML = count
+        if(idBook === bookSelected.asin)
+
+        cartList.innerHTML= ''
+
+        
+
+        const totalCardCount = function totalCard () {
+            count++
+            console.log(count)
+            cartCount =  document.querySelectorAll('.badge')
+        for (const cart of cartCount) {
+            cart.innerHTML = count
 
 
 
-//         }
-//     })
-// })
+        }
+    }
 
-window.onload = function () {
-    getBooks()
+    totalCardCount()
+        
+  
+        
+         cartList.innerHTML +=   /*html */`
+                 <li class="list-group-item d-flex justify-content-between align-items-center border border-0">
+                    <div class="card-book card mb-3" style="max-width: 540px;">
+                        <div class="row row-cols-3 g-0">
+                
+                            <div class="col-md-2">
+                                <img src="${bookSelected.img}" class="product-img img-fluid rounded-start" alt="">
+                             </div>
+                             <div class="col card-body d-flex flex-column justify-content-center align-items-end p-0">
+                                 <h5 class="product-title card-title fs-6 m-0">${bookSelected.title}</h5>
+                
+                                 <p class="product-price card-text">${bookSelected.price}</p>
+                             </div>
+                
+                             <div class="col d-flex align-items-center justify-content-center">
+                                 <button class="btn btn-outline-secondary p-0" type="button"><i class="bi bi-dash-lg"></i></button> 
+                                 <span class="badge text-bg-light bg-transparent">${count}</span>
+                                 <button class="btn btn-outline-secondary p-0" type="button"><i class="bi bi-plus-lg"></i></button>
+                             </div>
+                         </div>
+                     </div>
+                 </li>        
+                 `
+
+
+    })
 }
+
+
+
+
+
+
+
 
 // funzione di chiamata di tutti i libri
 
@@ -42,22 +91,21 @@ function getBooks() {
 
             booksResult = result
 
-
-            const renderedResult = booksResult.map(result => {
+            renderedResult = booksResult.map(result => {
                 return /*html*/`
                             <div class="col-md-3">
                                 <div class="card-book_ card h-100 border border-0">
                                 <a href="book.html?id=${result.asin}">
                                 <img src="${result.img}" class="card-img-top" alt="...">
                                     <div class="card-body">
-                                        <h5 class="card-title text-center"> <a href="artist.html?id=${result.asin}">
-                                        ${result.title}</h5>
+                                        <h5 class="card-title text-center"> <a href="book.html?id=${result.asin}" class="link-underline link-underline-opacity-0 link-dark">
+                                        ${result.title}</a></h5>
                                         <p class="card-text text-center">${result.price}€</p>
                         
                                     </div>
                                     <div class= "cart-overlay bg-light d-flex justify-content-evenly">
                                         <button type="button" class="btn btn-transparent"> <i class="bi bi-suit-heart fs-3"></i></button>
-                                        <button type="button" class="cartButton btn btn-transparent" onclick="addToCart('${result.title}', '${result.price}', '${result.asin}','${result.img}')">  <i class="bi bi-cart2 fs-3"></i> </button>
+                                        <button type="button" class="cartButton btn btn-transparent" data-asin='${result.asin}' >  <i class="bi bi-cart2 fs-3"></i> </button>
                                     </div>
                                 </div>
                             </div>
@@ -67,86 +115,94 @@ function getBooks() {
                         `
 
 
-            })
-            cardRow.innerHTML += renderedResult.join('')
+            }).join('')
+            cardRow.innerHTML += renderedResult
             inputSearch = document.querySelector('#inputSearch')
             cardRow = document.querySelector('#cardRow')
             addItems = document.querySelectorAll('.cartButton')
             cartCount = document.querySelectorAll('.badge')
 
+            addItems.forEach(addToCart)
 
         })
         .catch(error => console.log('error', error));
 }
 
 
+getBooks()
 
 
 
-function addToCart(title, price, asin, img) {
-    const book = document.querySelector("#card-book_" + asin)
-    let cartList = document.querySelector('#cartList')
-    let count = 0
-    addItems.forEach(button => {
+// function addToCart(title, price, asin, img) {
+//     const book = document.querySelector("#card-book_" + asin)
+//     let cartList = document.querySelector('#cartList')
+//     cartCount = document.querySelectorAll('.badge')
 
-        let count = 0
-        let totalPrice = 0
-        button.addEventListener('click', () => {
+//     let count = 0
+//     addItems.forEach(buttons => {
+
+//         let count = 0
+//         let totalPrice = 0
+//         buttons.addEventListener('click', () => {
+
+//             for (const buttonCart of buttons) {
+                
             
-            if (book)
-            cartList.innerHTML =''
-
-            cartCount = document.querySelectorAll('.badge')
-            count++
+//                 count++
+//                 buttonCart.innerHTML = count
+            
+//             }
                 
 
-            
-
-            cartCount.innerHTML = event
-        }
-
-
-        )
-    })
 
 
 
 
+           
+//         }
+
+
+//         )
+//     })
+
+
+
+//     cartList.innerHTML += /*html */`
+//                 <li class="list-group-item d-flex justify-content-between align-items-center border border-0">
+//                     <div class="card-book card mb-3" style="max-width: 540px;">
+//                         <div class="row row-cols-3 g-0">
+                
+//                             <div class="col-md-2">
+//                                 <img src="${img}" class="product-img img-fluid rounded-start" alt="">
+//                             </div>
+//                             <div class="col card-body d-flex flex-column justify-content-center align-items-end p-0">
+//                                 <h5 class="product-title card-title fs-6 m-0">${title}</h5>
+                
+//                                 <p class="product-price card-text">${price}</p>
+//                             </div>
+                
+//                             <div class="col d-flex align-items-center justify-content-center">
+//                                 <button class="btn btn-outline-secondary p-0" type="button"><i class="bi bi-dash-lg"></i></button> 
+//                                 <span class="badge text-bg-light bg-transparent">${count}</span>
+//                                 <button class="btn btn-outline-secondary p-0" type="button"><i class="bi bi-plus-lg"></i></button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </li>        
+//                 `
+
+
+
+
+//     const totale = document.querySelector("h3 span")
+//     totale.innerText = (Number(totale.innerText) + Number(price)).toFixed(2)
+// }
 
 
 
 
 
-    cartList.innerHTML += `
-            <li class="list-group-item d-flex justify-content-between align-items-center border border-0">
-                <div class="card-book card mb-3" style="max-width: 540px;">
-                    <div class="row row-cols-3 g-0">
-            
-                        <div class="col-md-2">
-                            <img src="${img}" class="product-img img-fluid rounded-start" alt="">
-                        </div>
-                        <div class="col card-body d-flex flex-column justify-content-center align-items-end p-0">
-                            <h5 class="product-title card-title fs-6 m-0">${title}</h5>
-            
-                            <p class="product-price card-text">${price}</p>
-                        </div>
-            
-                        <div class="col d-flex align-items-center justify-content-center">
-                            <button class="btn btn-outline-secondary p-0" type="button"><i class="bi bi-dash-lg"></i></button> 
-                            <span class="badge text-bg-light bg-transparent">${count}</span>
-                            <button class="btn btn-outline-secondary p-0" type="button"><i class="bi bi-plus-lg"></i></button>
-                        </div>
-                    </div>
-                </div>
-            </li>        
-            `
 
-
-
-
-    const totale = document.querySelector("h3 span")
-    totale.innerText = (Number(totale.innerText) + Number(price)).toFixed(2)
-}
 
 
 
